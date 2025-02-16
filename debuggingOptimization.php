c2a9ea7f4fd4f96ac2ab84600f93698f7f5f5fb5
Jay Prakash Gupta <jaygupta9584@gmail.com>
2025-02-16 10:14:35 +0530

    Debugging & Optimization

--- debuggingOptimization.php
+++ debuggingOptimization.php
@@ -1,5 +1,17 @@
 <?php
 
-print_r("Hello World");
+function getUserPosts($userId, PDO $db) {
+    $stmt = $db->prepare("SELECT * FROM posts WHERE user_id = :user_id");
+    $stmt->bindParam(':user_id', $userId, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+$db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+$userPosts = getUserPosts(1, $db);
+
+echo '<pre>';
+print_r($userPosts);
+echo '</pre>';
 
 ?>
